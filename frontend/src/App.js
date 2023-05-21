65f537f24bcd6234567b49b9a9e6e66847424837
deadcrow2021 <majorpayne2021@gmail.com>
2023-05-21 14:14:26 +0300

    add styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,12 +12,14 @@ import {
 function App() {
   return (
     <Router>
-      <div className='App'>
-        <Header />
-        <Routes>
-          <Route path='/' exact Component={NoteListPage}/>
-          <Route path='/note/:id' Component={NotePage}/>
-        </Routes>
+      <div className='container dark'>
+        <div className='app'>
+          <Header />
+          <Routes>
+            <Route path='/' exact Component={NoteListPage}/>
+            <Route path='/note/:id' Component={NotePage}/>
+          </Routes>
+        </div>
       </div>
     </Router>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app-header'>
+      <h1>
+        Note List
+      </h1>
+    </div>
   )
 }
 
